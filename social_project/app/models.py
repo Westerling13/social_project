37578dd77a4b2f34f3243bbd0880ba3a7e9323b1
Alex Munz <a_munz@list.ru>
2020-02-09 18:58:20 +0300

    Add Profile model

--- social_project/app/models.py
+++ social_project/app/models.py
@@ -1,3 +1,40 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.urls import reverse
+from django.utils import timezone
 
-# Create your models here.
+
+class Profile(models.Model):
+    """Модель профиля пользователя"""
+    GENDERS = [
+        (None, 'Выберите пол'),
+        ('Мужской', 'Мужской'),
+        ('Женский', 'Женский'),
+    ]
+
+    user = models.OneToOneField(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+    image = models.ImageField('Аватар', upload_to='users/', default='placeholder-200.png')
+    gender = models.CharField('Пол', choices=GENDERS, blank=False, default=None, max_length=7, null=True)
+    birth_date = models.DateField('Дата рождения', null=True, default=timezone.now)
+    bio = models.TextField('Личная информация', max_length=1000)
+    is_private = models.BooleanField('Приватный', default=False)
+
+    def __str__(self):
+        return self.user.username
+
+    def get_absolute_url(self):
+        return reverse('user', args=[self.user.username])
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
+
+
+@receiver(post_save, sender=User)
+def update_profile_signal(sender, instance, created, **kwargs):
+    """Сигнал на создание профиля пользователя при регистрации"""
+    if created:
+        Profile.objects.create(user=instance)
+        instance.profile.save()
