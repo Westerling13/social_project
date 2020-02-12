78b8bb4031e3817d5aa12bb8e5be62142a77fa7f
Alex Munz <a_munz@list.ru>
2020-02-12 14:58:25 +0300

    Fixes

--- social_project/app/forms.py
+++ social_project/app/forms.py
@@ -1,14 +1,22 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from django.core.exceptions import ValidationError
 
 from .models import Story, Profile, Comment
 
 
 class SignUpForm(UserCreationForm):
+    # username = forms.CharField(label=)
     first_name = forms.CharField(label='Имя')
     last_name = forms.CharField(label='Фамилия')
     email = forms.EmailField(label='Электронный адрес')
+
+    def clean(self):
+        super().clean()
+        username = self.cleaned_data['username']
+        if '@' in username:
+            raise ValidationError('Извините, имя пользователя не может содержать @')
 
     class Meta:
         model = User
--- social_project/app/templatetags/app_tags.py
+++ social_project/app/templatetags/app_tags.py
@@ -4,5 +4,5 @@ register = template.Library()
 
 
 @register.inclusion_tag('layout/base_tag.html', takes_context=True)
-def nav(context, template='nav.html', page='index'):
+def nav(context, template='nav.html', page=None):
     return {'context': context, 'template': template, 'page': page}
